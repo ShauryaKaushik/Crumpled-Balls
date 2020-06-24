fc01dfddfd93779fa786380a73760a65503c53cc
ShauryaKaushik <65485931+ShauryaKaushik@users.noreply.github.com>
2020-06-24 11:32:09 +0530

    Update PaperBall.js

--- PaperBall.js
+++ PaperBall.js
@@ -1,12 +1,12 @@
 class PaperBall{
-    constructor(x, y, width, height) {
+    constructor(x, y) {
         var options = {
             "isStatic" : false,
             'restitution':0.3,
             'friction':0.5,
             'density':1.2
         }
-        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.body = Bodies.rectangle(x, y, options);
         this.width = width;
         this.height = height;
         World.add(world, this.body);
